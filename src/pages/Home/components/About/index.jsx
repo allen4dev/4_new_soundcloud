9be8fb66a60f5b4c58f364b7dc9fa5867be62f81
allen4dev <alanaliagadev@gmail.com>
2017-11-05 19:14:29 -0400

    [Add] About component in Home (Explore)

--- src/pages/Home/components/About/index.jsx
+++ src/pages/Home/components/About/index.jsx
@@ -4,8 +4,16 @@ import './index.css';
 
 const About = () => {
   return (
-    <div className="About">
-      <h1>About</h1>
+    <div className="About container">
+      <div className="About-content">
+        <h2 className="About-title">Llamada a todos los creadores</h2>
+        <p className="About-text">
+          Hazte con SoundCloud para estar en contacto con tus seguidores,
+          compartir tus canciones y aumentar tu publico. ¿A que esperas?
+        </p>
+
+        <button className="About-button btn btn-flat">Mas informacion</button>
+      </div>
     </div>
   );
 };
