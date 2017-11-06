90ed5f0a09173003b1e0c7908784c967de7822fe
allen4dev <alanaliagadev@gmail.com>
2017-11-06 19:33:09 -0400

    [Add] TrackHeader in tracks feature component

---
+++ src/pages/TrackDetail/components/Info/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './index.css';
+
+const Info = () => {
+  return (
+    <div className="TrackDetail-info">
+      <span>Info</span>
+    </div>
+  );
+};
+
+export default Info;
--- src/pages/TrackDetail/index.jsx
+++ src/pages/TrackDetail/index.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
+import TrackHeader from './../../modules/tracks/components/TrackHeader';
+
+import Info from './components/Info';
+
 import './index.css';
 
 export class TrackDetail extends Component {
   render() {
     return (
-      <div className="TrackDetail">
-        <h1>TrackDetail</h1>
-      </div>
+      <section className="TrackDetail page">
+        <TrackHeader />
+        <Info />
+      </section>
     );
   }
 }
