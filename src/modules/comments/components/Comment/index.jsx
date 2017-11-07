7fd9b3b9c1bf4520ddac598228391ba8540a596e
allen4dev <alanaliagadev@gmail.com>
2017-11-07 11:50:44 -0400

    [Add] Feedback shared component

--- src/modules/comments/components/Comment/index.jsx
+++ src/modules/comments/components/Comment/index.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 
 import './index.css';
 
+import defaultImage from './../../../../images/default_image.png';
+
 const Comment = () => {
+  const src = defaultImage;
+
   return (
     <li className="Comment">
-      <span>Comment here</span>
+      <div className="Comment-avatar imageContainer">
+        <img src={src} alt="user avatar" className="Comment-image image" />
+      </div>
+
+      <div className="Comment-description">
+        <div className="Comment-info">
+          <span className="Comment-author">allen4dev</span>
+          <span className="Comment-publishedAt">9 minutos</span>
+        </div>
+        <p className="Comment-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo,
+          sapiente repudiandae! Dignissimos, labore. Eum sunt commodi quod ipsa
+          mollitia. Quidem ab debitis dolor blanditiis sit consequuntur earum
+          modi doloremque consectetur!
+        </p>
+      </div>
     </li>
   );
 };
--- src/shared/Feedback/index.jsx
+++ src/shared/Feedback/index.jsx
@@ -11,12 +11,12 @@ const Feedback = () => {
       <UserCard />
       <div className="Feedback-content">
         <div className="Feedback-itemInfo">
-          <div className="Feedback-itemRelease">
+          <div className="Feedback-itemBox">
             <h4 className="Feedback-itemTitle">Fecha de lanzamiento</h4>
             <p className="Feedback-text">15 Septiembre 2017</p>
           </div>
 
-          <div className="Feedback-itemAnbout">
+          <div className="Feedback-itemBox">
             <h4 className="Feedback-itemTitle">Linea de comunicacion</h4>
             <p className="Feedback-text">
               © 2017 Republic Records, a division of UMG Recording, Inc.
