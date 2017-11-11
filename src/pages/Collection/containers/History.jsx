a74cfcbecbb67cbff9012323f0b02b101a3f793e
allen4dev <alanaliagadev@gmail.com>
2017-11-10 23:10:18 -0400

    [Add] Collection (History)

--- src/pages/Collection/containers/History.jsx
+++ src/pages/Collection/containers/History.jsx
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import TrackRowList from './../../../modules/tracks/components/TrackRowList';
 
+import tracks from './../../../modules/tracks';
+
 export class History extends Component {
+  renderContent() {
+    if (this.props.items.length === 0) {
+      return <span>No tracks listened</span>;
+    }
+
+    return (
+      <TrackRowList
+        items={this.props.items}
+        handlePlay={this.handlePlay}
+        handleAdd={this.handleAdd}
+      />
+    );
+  }
+
+  // Refactor
+  handleAdd = id => {
+    const { items, addToTracklist } = this.props;
+    const isInPlaylist = items.findIndex(trackId => trackId === id);
+    if (isInPlaylist === -1) {
+      addToTracklist(id);
+    }
+  };
+
+  handlePlay = id => {
+    // Refactor: Dont dispatch if the track currently exists
+    this.props.setCurrentTrack(id);
+  };
+
   render() {
     return (
       <section className="Collection-history">
@@ -10,12 +41,20 @@ export class History extends Component {
           Escucha las pista que has reproducido:
         </h3>
 
-        <span>No tracks listened</span>
+        <div className="Collection-historyContent">{this.renderContent()}</div>
       </section>
     );
   }
 }
 
-/* <TrackRowList items={new Array(12).fill({})} /> */
+function mapStateToProps(state) {
+  const ids = state.tracks.history;
 
-export default History;
+  return {
+    items: ids.map(id => state.tracks.entities[id]),
+  };
+}
+
+export default connect(mapStateToProps, {
+  addToTracklist: tracks.actions.addToTracklist,
+})(History);
